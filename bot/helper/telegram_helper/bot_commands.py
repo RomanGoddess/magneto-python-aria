2a5df21112b514f25072cf96e3765781253ad768
Utopia <53878781+RomanGoddess@users.noreply.github.com>
2020-09-03 08:01:32 +0100

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,18 +1,18 @@
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
-        self.MirrorCommand = 'megadownload'
+        self.MirrorCommand = 'megamirror'
         self.UnzipMirrorCommand = 'unzipmirror'
         self.TarMirrorCommand = 'tarmirror'
         self.CancelMirror = 'megacancel'
         self.CancelAllCommand = 'cancelall'
         self.ListCommand = 'list'
-        self.StatusCommand = 'status'
+        self.StatusCommand = 'megastatus'
         self.AuthorizeCommand = 'authorize'
         self.UnAuthorizeCommand = 'unauthorize'
         self.PingCommand = 'ping'
         self.RestartCommand = 'restart'
-        self.StatsCommand = 'stats'
+        self.StatsCommand = 'megastats'
         self.HelpCommand = 'help'
         self.LogCommand = 'log'
         self.CloneCommand = "clone"
